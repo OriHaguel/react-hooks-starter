324b3926e0bde3b218dbf2cefa3f8e157fcc8f44
OriHaguel <orihaguel@gmail.com>
2024-05-26 20:53:44 +0300

    Add stars works and styling

--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,4 +1,5 @@
 const { useParams, useNavigate, Link, useSearchParams } = ReactRouterDOM
+const { useState, useEffect } = React
 import { emailService } from "../../../apps/mail/services/mail.service.js"
 import { LongTxt } from "../../../cmps/LongTxt.jsx"
 
@@ -7,9 +8,11 @@ import { utilService } from "../../../services/util.service.js"
 
 
 export function MailPreview({ mail, onSelect }) {
+    const [starClicked, setStarClicked] = useState(false);
 
-
-
+    function onStar() {
+        setStarClicked(prevStar => !prevStar)
+    }
     // function onSelect() {
 
     //     navigate(`/mail/${mail.id}`)
@@ -20,8 +23,15 @@ export function MailPreview({ mail, onSelect }) {
     // }
     const dateDetails = utilService.getDateDetails(mail.sentAt)
     return <React.Fragment>
-        <td><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#D9D9D9"><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z" /></svg></td>
-        <td className="input-td"><input type="checkbox" /></td>
+
+        <td className="input-td">
+            <input type="checkbox" />
+
+            {starClicked ? <svg onClick={onStar} className="star-clicked-icon" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="20px" viewBox="0 0 24 24" width="20px" fill="#F4B400"><g><path d="M0 0h24v24H0V0z" fill="none" /><path d="M0 0h24v24H0V0z" fill="none" /></g><g><path d="M12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27z" /></g></svg> : <svg onClick={onStar} className="star-icon" xmlns="http://www.w3.org/2000/svg" height="20px" viewBox="0 -960 960 960" width="20px" fill='#D9D9D9'><path d="m354-287 126-76 126 77-33-144 111-96-146-13-58-136-58 135-146 13 111 97-33 143ZM233-120l65-281L80-590l288-25 112-265 112 265 288 25-218 189 65 281-247-149-247 149Zm247-350Z" /></svg>}
+
+
+
+        </td>
         <td className="text-container" onClick={() => onSelect(mail)}>
             <span className={`span-text sub ${!mail.isRead && 'subject'}`}> {mail.subject}</span>
             {/* <span>{mail.body}</span> */}
